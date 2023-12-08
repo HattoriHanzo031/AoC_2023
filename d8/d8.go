34164dcfa472e981657af47857b8fc4bf463c909
HattoriHanzo031 <filip031@gmail.com>
2023-12-08 13:55:10 +0100

    d8 p2

--- d8/d8.go
+++ d8/d8.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 	"utils"
 )
 
 func main() {
+	defer utils.Profile(time.Now())
+
 	scanner, cleanup := utils.FileScaner("d8/input.txt")
 	defer cleanup()
 
@@ -18,13 +21,19 @@ func main() {
 			instructions[i] = 1
 		}
 	}
-
 	scanner.Scan()
 
+	lanes := []string{}
+	endingPoints := make(map[string]bool)
 	nodes := make(map[string][]string)
 	for scanner.Scan() {
 		ss := strings.Split(strings.TrimSuffix(scanner.Text(), ")"), " = (")
 		nodes[ss[0]] = strings.Split(ss[1], ", ")
+		if ss[0][2] == 'A' {
+			lanes = append(lanes, ss[0])
+		} else if ss[0][2] == 'Z' {
+			endingPoints[ss[0]] = true
+		}
 	}
 
 	cur := "AAA"
@@ -32,5 +41,17 @@ func main() {
 	for ; cur != "ZZZ"; turn++ {
 		cur = nodes[cur][instructions[turn%len(instructions)]]
 	}
-	fmt.Println(turn)
+	fmt.Println("P1:", turn)
+
+	periods := []int{}
+	for turn = 0; len(periods) < len(lanes); turn++ {
+		for i := 0; i < len(lanes); i++ {
+			lanes[i] = nodes[lanes[i]][instructions[turn%len(instructions)]]
+			if endingPoints[lanes[i]] {
+				periods = append(periods, turn+1)
+			}
+		}
+	}
+
+	fmt.Println("P2:", utils.LCM(periods[0], periods[1], periods[2:]...))
 }
--- utils/utils.go
+++ utils/utils.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
 func Must[T any](v T, err error) T {
@@ -28,4 +30,29 @@ func ToInts(ss []string) []int {
 		ints = append(ints, Must(strconv.Atoi(s)))
 	}
 	return ints
+}
+
+// greatest common divisor (GCD) via Euclidean algorithm
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
+
+func Profile(start time.Time) {
+	fmt.Println(time.Since(start))
 }
