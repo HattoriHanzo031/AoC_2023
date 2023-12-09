efdd0a0a1947bcb7d68efaebe027f7c731c8f3bc
HattoriHanzo031 <filip031@gmail.com>
2023-12-09 18:38:54 +0100

    d9 p2

--- d9/d9.go
+++ d9/d9.go
@@ -7,36 +7,46 @@ import (
 	"utils"
 )
 
+func alternateAddSubFn() func(a, b int) int {
+	invocation := 0
+	return func(a, b int) int {
+		invocation++
+		return []func(a, b int) int{
+			func(a, b int) int { return a - b },
+			func(a, b int) int { return a + b },
+		}[invocation%2](a, b)
+	}
+}
+
 func main() {
 	defer utils.Profile(time.Now())
-	scanner, cleanup := utils.FileScaner("d9/test.txt")
+	scanner, cleanup := utils.FileScaner("d9/input.txt")
 	defer cleanup()
 
 	histories := [][]int{}
 	for scanner.Scan() {
 		histories = append(histories, utils.ToInts(strings.Fields(scanner.Text())))
 	}
-	//fmt.Println(histories)
 
-	total := 0
+	totalP1 := 0
+	totalP2 := 0
 	for _, history := range histories {
-		prediction := history[len(history)-1]
-		fmt.Println(history)
+		fn := alternateAddSubFn()
+		totalP1 += history[len(history)-1]
+		totalP2 = fn(totalP2, history[0])
 		for sum := -1; sum != 0; {
 			sum = 0
-			newHistory := []int{}
 			for i := 0; i < len(history)-1; i++ {
-				newHistory = append(newHistory, history[i+1]-history[i])
-				//history[i] = history[i+1] - history[i]
 				sum += history[i]
+				history[i] = history[i+1] - history[i]
 			}
-			//history = history[:len(history)-1]
-			//fmt.Println(history)
-			prediction += newHistory[len(newHistory)-1]
-			history = newHistory
+			history = history[:len(history)-1]
+
+			totalP1 += history[len(history)-1]
+			totalP2 = fn(totalP2, history[0])
 		}
-		fmt.Println(prediction)
-		total += prediction
 	}
-	fmt.Println(total)
+
+	fmt.Println("P1:", totalP1)
+	fmt.Println("P2:", totalP2)
 }
