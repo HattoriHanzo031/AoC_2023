eeec4ae889083ab6d1ab9cb30b0da774b8e4c4d4
HattoriHanzo031 <filip031@gmail.com>
2023-12-16 08:36:15 +0100

    d14 p2

--- d14/d14.go
+++ d14/d14.go
@@ -2,44 +2,98 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"slices"
 	"time"
 	"utils"
 )
+
+func print(input [][]byte) {
+	for _, line := range input {
+		fmt.Println(string(line))
+	}
+	fmt.Println("")
+}
+
+func rotate(input [][]byte) {
+	for i := 0; i < len(input)-1; i++ {
+		for j := 0; j < len(input[i])-1-i; j++ {
+			input[i][j], input[len(input[i])-1-j][len(input)-1-i] = input[len(input[i])-1-j][len(input)-1-i], input[i][j]
+		}
+	}
+	slices.Reverse(input)
+}
+
+func slide(input [][]byte) {
+	for i, line := range input {
+		storeRoundTo := len(line) - 1
+		for j := len(line) - 1; j >= 0; j-- {
+			switch line[j] {
+			case 'O':
+				input[i][storeRoundTo] = 'O'
+				if j != storeRoundTo {
+					input[i][j] = '.'
+				}
+				storeRoundTo--
+			case '#':
+				storeRoundTo = j - 1
+			}
+		}
+	}
+}
+
+func cycle(input [][]byte) {
+	for i := 0; i < 4; i++ {
+		rotate(input)
+		slide(input)
+	}
+}
+
+func hash(input [][]byte) string {
+	key := []byte{}
+	for i, line := range input {
+		for j, c := range line {
+			if c == 'O' {
+				key = append(key, byte(i), byte(j))
+			}
+		}
+	}
+	return string(key)
+}
+
+func totalLoad(input [][]byte) int {
+	total := 0
+	for j := 0; j < len(input[0]); j++ {
+		for i := 0; i < len(input); i++ {
+			if input[i][j] == 'O' {
+				total += len(input) - i
+			}
+		}
+	}
+	return total
+}
 
 func main() {
 	defer utils.Profile(time.Now())
 	scanner, cleanup := utils.FileScaner("d14/input.txt")
 	defer cleanup()
 
-	var input []string
+	var input [][]byte
 	for scanner.Scan() {
-		if input == nil {
-			input = make([]string, len(scanner.Text()))
-		}
-		for i, b := range scanner.Text() {
-			input[i] += string(b)
-		}
+		input = append(input, slices.Clone(scanner.Bytes()))
 	}
 
-	for i, line := range input {
-		out := []string{}
-		for _, s := range strings.Split(line, "#") {
-			count := strings.Count(s, ".")
-			out = append(out, strings.ReplaceAll(s, ".", "")+strings.Repeat(".", count))
+	cycleDetection := make(map[string]int)
+	for i := 0; i < 1000_000_000; i++ {
+		cycle(input)
+
+		key := hash(input)
+		if prev, found := cycleDetection[key]; found {
+			cycleLen := i - prev
+			// skip all the cycles
+			i = 1000_000_000 - ((1000_000_000 - i) % cycleLen)
 		}
-		input[i] = strings.Join(out, "#")
+		cycleDetection[key] = i
 	}
 
-	total := 0
-	for j := 0; j < len(input[0]); j++ {
-		for i := 0; i < len(input); i++ {
-			fmt.Print(string(input[i][j]))
-			if input[i][j] == 'O' {
-				total += len(input[0]) - j
-			}
-		}
-		fmt.Println("")
-	}
-	fmt.Println(total)
+	fmt.Println("P2:", totalLoad(input))
 }
