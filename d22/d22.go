72bebebdadc8942ecff06402b71e8b55d39fff9d
HattoriHanzo031 <filip031@gmail.com>
2023-12-22 15:49:48 +0100

    simpler but less optimal solution

--- d22/d22.go
+++ d22/d22.go
@@ -11,7 +11,6 @@ import (
 type coord struct{ x, y int }
 
 type brick struct {
-	num        int
 	blocks     []coord
 	minZ, maxZ int
 }
@@ -24,7 +23,7 @@ func main() {
 	// 1,0,1~1,2,1
 	bricks := []brick{}
 	for i := 0; scanner.Scan(); i++ {
-		b := brick{num: i}
+		b := brick{}
 		s, e, _ := strings.Cut(scanner.Text(), "~")
 		start := utils.ToInts(strings.Split(s, ","))
 		end := utils.ToInts(strings.Split(e, ","))
@@ -37,55 +36,27 @@ func main() {
 		bricks = append(bricks, b)
 	}
 
-	slices.SortFunc(bricks, func(a, b brick) int {
-		return a.minZ - b.minZ
-	})
-
-	type surface struct{ z, brickNum int }
-
-	surfaces := map[coord]surface{}
-	doNotDisintegrate := map[int]bool{}
-	for i, brick := range bricks {
-		dropTo := 0
-		for _, bl := range brick.blocks {
-			dropTo = max(dropTo, surfaces[bl].z)
-		}
-		bricks[i].maxZ = bricks[i].maxZ - (bricks[i].minZ - dropTo)
-		bricks[i].minZ = dropTo
-
-		supportedBy := []int{}
-		for _, bl := range brick.blocks {
-			if s, ok := surfaces[bl]; ok && s.z == dropTo {
-				supportedBy = append(supportedBy, s.brickNum)
-			}
-			surfaces[bl] = surface{bricks[i].maxZ, bricks[i].num}
-		}
-		slices.Sort(supportedBy)
-		supportedBy = slices.Compact(supportedBy)
-
-		if len(supportedBy) == 1 {
-			doNotDisintegrate[supportedBy[0]] = true
-		}
-	}
-	slices.SortFunc(bricks, func(a, b brick) int {
-		return a.minZ - b.minZ
-	})
+	drop(bricks) // initial drop
 
 	totalP1 := 0
 	totalP2 := 0
-	for i, b := range bricks {
-		if !doNotDisintegrate[b.num] {
+	for i := range bricks {
+		dropped := drop(slices.Delete(slices.Clone(bricks), i, i+1))
+		if dropped == 0 {
 			totalP1++
-		} else {
-			totalP2 += drop(slices.Delete(slices.Clone(bricks), i, i+1))
 		}
+		totalP2 += dropped
 	}
-
 	fmt.Println("P1:", totalP1)
 	fmt.Println("P2:", totalP2)
 }
 
 func drop(bricks []brick) int {
+	// no need to sort each time, but sorting sorted slice is very fast
+	slices.SortFunc(bricks, func(a, b brick) int {
+		return a.minZ - b.minZ
+	})
+
 	dropped := 0
 	surfaces := map[coord]int{}
 	for i, brick := range bricks {
